531fd1d42bf2fd6ebbb7c5cd9c0b6435df49e621
Marvin.Min <xiaorong.min@gmail.com>
2023-12-11 22:49:19 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { useEffect, useRef, useState } from 'react';
+import _ from 'lodash';
 import './App.css';
 
 function App() {
+  const heroRef = useRef();
+  function viewportResize() {
+    const composerVH = window.visualViewport.height,
+        doc = document.documentElement;
+    console.log(window.visualViewport);
+    doc.style.setProperty("--composer-vh", `${composerVH}px`);
+  }
+  useEffect(()=>{
+    window.visualViewport.addEventListener('resize', viewportResize);
+    heroRef.current.style.setProperty("--hero-image", `url(${'https://www.active.com/asset_pipeline/hero/a3-hero-c25k-ec8d82aaf12ad1dff2655f67baae58bf005e013246bc223c3120f507907c7d02.jpg'})`);
+    return () => window.removeEventListener('resize', viewportResize);
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home" ref={heroRef}>
+      <div className="fixed-element">
+        <div className="textarea">
+            <textarea>Steps to reproduce:
+                &#13;&#10;1. Open this page in Safari iOS 15
+                &#13;&#10;2. Tap the textarea (result: the floating address bar hides the submit button when the keyboard is visible)
+                &#13;&#10;3. Update Safari settings, set address bar to be at the top
+                &#13;&#10;4. Tap textarea again (result: submit button is visible, correctly)
+            </textarea>
+        </div>
+        <div className="button">
+            <button>Submit</button>
+        </div>
+    </div>
     </div>
   );
 }
